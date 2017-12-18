a064a161b684d328d29d00f7a053191cd3f6c69f
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2017-12-18 23:47:02 +0700

    sudah mulai ada pencerahan

--- app/src/main/java/com/example/bismillah/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bismillah/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
-
 import java.util.ArrayList;
 import java.util.List;
 
